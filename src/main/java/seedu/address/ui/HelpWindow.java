4012e3a7323e954e52df3623bc3381327cd77d4c
zhengzikang <zhengzikangzzz@gmail.com>
2022-04-02 16:44:48 +0800

    Fix help window UG link

--- src/main/java/seedu/address/ui/HelpWindow.java
+++ src/main/java/seedu/address/ui/HelpWindow.java
@@ -16,7 +16,7 @@ import seedu.address.commons.core.LogsCenter;
 public class HelpWindow extends UiPart<Stage> {
 
     public static final String USERGUIDE_URL =
-            "https://github.com/AY2122S2-CS2103-F09-3/tp/blob/master/docs/UserGuide.md";
+            "https://ay2122s2-cs2103-f09-3.github.io/tp/UserGuide.html#delete-insurance";
     public static final String HELP_MESSAGE = "Refer to the user guide: " + USERGUIDE_URL;
 
     private static final Logger logger = LogsCenter.getLogger(HelpWindow.class);
